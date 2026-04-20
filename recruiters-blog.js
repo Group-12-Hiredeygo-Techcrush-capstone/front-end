bd6f31e8e0133f18cba9b4c7a5c69f0be02596f1
Benedict <adibelibenedict1@gmail.com>
2026-04-20 17:54:17 +0100

    made the buttons clickable using javaScript functions

--- recruiters-blog.js
+++ recruiters-blog.js
@@ -4,3 +4,28 @@ btn.addEventListener("click", () =>{
 });
 
 console.log("hello world")
+
+const explore = document.querySelector(".read-articles-btn");
+explore.addEventListener("click",() =>{
+     window.location.href ="home.html"
+    
+});
+ if("explore===active"){
+        console.log("active")
+     }else{
+        console.log("not active")
+     }
+console.log("it has been clicked")
+
+
+
+const subscribe = document.querySelector(".subscribe-btn");
+subscribe.addEventListener("click", () =>{
+    window.location.href = "subscribe.html"
+})
+
+if("subscribe ===true"){
+    console.log("logged in")
+}else{
+    console.log("not logged in")
+}
